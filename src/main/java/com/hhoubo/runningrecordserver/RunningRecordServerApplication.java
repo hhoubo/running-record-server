02e826e5bf3663812aa0b82d297c400bbd18b551
Bo Hou <hou_b@worksap.co.jp>
2017-06-29 20:10:13 +0900

    create a project

---
+++ src/main/java/com/hhoubo/runningrecordserver/RunningRecordServerApplication.java
@@ -0,0 +1,12 @@
+package com.hhoubo.runningrecordserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningRecordServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningRecordServerApplication.class, args);
+	}
+}
